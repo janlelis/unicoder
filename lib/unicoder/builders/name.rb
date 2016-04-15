b5227a5ec5eea1fba5cb135bb943c7b63ccef0c4
Jan Lelis <mail@janlelis.de>
2016-04-16 00:22:48 +0200

    Name builder: Support CJK

--- lib/unicoder/builders/name.rb
+++ lib/unicoder/builders/name.rb
@@ -7,12 +7,30 @@ module Unicoder
         @index = {
           NAMES: {},
           ALIASES: {},
+          CJK: [],
+          HANGUL: [],
         }
+        @range_start = nil
       end
 
       def parse!
         parse_file :unicode_data, :line, regex: /^(?<codepoint>.+?);(?<name>.+?);.*$/ do |line|
-          unless line["name"][0] == "<" && line["name"][-1] == ">"
+          if line["name"][0] == "<" && line["name"][-1] == ">"
+            if line["name"] =~ /First/
+              @range_start = line["codepoint"].to_i(16)
+            elsif line["name"] =~ /Last/ && @range_start
+              if line["name"] =~ /Hangul/
+                @index[:HANGUL] << [@range_start, line["codepoint"].to_i(16)]
+              elsif line["name"] =~ /CJK/
+                @index[:CJK] << [@range_start, line["codepoint"].to_i(16)]
+              else
+                # no name
+              end
+              @range_start = nil
+            elsif line["name"] != "<control>"
+              raise ArgumentError, "inconsistent range found in data, don't know what to do"
+            end
+          else
             assign_codepoint line["codepoint"].to_i(16), line["name"], @index[:NAMES]
           end
         end
@@ -22,8 +40,6 @@ module Unicoder
           @index[:ALIASES][line["codepoint"].to_i(16)][line["type"].to_sym] ||= []
           @index[:ALIASES][line["codepoint"].to_i(16)][line["type"].to_sym] << line["alias"]
         end
-
-        p @index
       end
     end
   end
